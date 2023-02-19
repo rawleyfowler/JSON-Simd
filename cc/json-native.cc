949e8fa52ad9efc097ae172b761454a92b003bd3
Rawley Fowler <rawleyfowler@gmail.com>
2023-02-18 22:41:56 -0600

    start rewrite

--- cc/json-native.cc
+++ cc/json-native.cc
@@ -1,89 +1,78 @@
+#include <algorithm>
+#include <map>
 #include <cstdlib>
+#include <memory>
 #include <string>
 #include <string_view>
+#include <random>
+#include <sstream>
+
 #include "lib/simdjson/singleheader/simdjson.h"
 
-static void (*add_uint64)(const char* name, uint64_t val);
-static void (*add_int64)(const char* name, int64_t val);
-static void (*add_str)(const char* name, const char* val);
-static void (*add_bool)(const char* name, int val);
-static void (*add_double)(const char* name, double val);
-static void (*add_null)(const char* name);
-static void (*add_obj)(const char* name); // If name resides as child of Obj, append to obj
-static void (*add_array)(const char *name); // If name resides as child of Array, append to array
+namespace uuid {
+    static std::random_device              rd;
+    static std::mt19937                    gen(rd());
+    static std::uniform_int_distribution<> dis(0, 5);
+    static std::uniform_int_distribution<> dis2(7, 17);
+
+    std::string generate_uuid_v4() {
+        std::stringstream ss;
+        int i;
+        ss << std::hex;
+        for (i = 0; i < 8; i++) {
+            ss << dis(gen);
+        }
+        ss << "-";
+        for (i = 0; i < 4; i++) {
+            ss << dis(gen);
+        }
+        ss << "-4";
+        for (i = 0; i < 3; i++) {
+            ss << dis(gen);
+        }
+        ss << "-";
+        ss << dis2(gen);
+        for (i = 0; i < 3; i++) {
+            ss << dis(gen);
+        }
+        ss << "-";
+        for (i = 0; i < 12; i++) {
+            ss << dis(gen);
+        };
+        return ss.str();
+    }
+}
+
+static std::shared_ptr<std::map<std::string, simdjson::dom::element>> parsed_json;
 
 extern "C" {
-void set_type_funcs(void (*add_uint64_raku)(const char *name, uint64_t val),
-               void (*add_int64_raku)(const char *name, int64_t val),
-               void (*add_str_raku)(const char *name, const char *val),
-               void (*add_bool_raku)(const char *name, int val),
-               void (*add_double_raku)(const char *name, double val),
-               void (*add_null_raku)(const char *name),
-               void (*add_obj_raku)(const char *name),
-               void (*add_array_raku)(const char *name)) {
-  add_uint64 = add_uint64_raku;
-  add_int64 = add_int64_raku;
-  add_str = add_str_raku;
-  add_bool = add_bool_raku;
-  add_double = add_double_raku;
-  add_null = add_null_raku;
-  add_obj = add_obj_raku;
-  add_array = add_array_raku;
-}
+typedef struct parse_result {
+  const char *message;
+  const char *status;
+} parse_result;
+
+void delete_parsed_json(const char *uuid) {
+  auto pjson = *parsed_json.get();
+  pjson.erase(std::string(uuid));
 }
 
-static void convert_to_raku(const simdjson::dom::element &element, const std::string location) {
-  switch (element.type()) {
-  case simdjson::dom::element_type::ARRAY: {
-	add_array(location.c_str());
-	auto idx = 0;
-	for (simdjson::dom::element s : element) {
-	  convert_to_raku(s, location + "␟␝" + std::to_string(idx++) + "␝");
-	}
-	break;
-  }
-  case simdjson::dom::element_type::OBJECT: {
-	add_obj(location.c_str());
-	for (simdjson::dom::key_value_pair kv : simdjson::dom::object(element)) {
-	  convert_to_raku(kv.value, location + "␟" + std::string(kv.key));
-	}
-	break;
-  }
-  case simdjson::dom::element_type::INT64:
-	add_int64(location.c_str(), element.get_int64());
-	break;
-  case simdjson::dom::element_type::UINT64:
-	add_uint64(location.c_str(), element.get_uint64());
-	break;
-  case simdjson::dom::element_type::BOOL:
-	add_bool(location.c_str(), element.get_bool() ? 1 : 0);
-	break;
-  case simdjson::dom::element_type::STRING:
-	if (!element.get_string().error()) {
-	  const std::string str(element.get_string().value().data());
-	  add_str(location.c_str(), str.c_str());
-	}
-	break;
-  case simdjson::dom::element_type::DOUBLE:
-	add_double(location.c_str(), element.get_double());
-	break;
-  case simdjson::dom::element_type::NULL_VALUE:
-	add_null(location.c_str());
-	break;
-  }
-}
-
-extern "C" {
-int parse_json(const char* json) {
+const parse_result parse_json(const char *json) {
   const std::string str(json);
   simdjson::dom::element doc;
   simdjson::dom::parser parser;
   auto error = parser.parse(str).get(doc);
+  parse_result result;
   if (error == simdjson::SUCCESS) {
-	convert_to_raku(doc, "");
-	return EXIT_SUCCESS;
+    std::string uuid = uuid::generate_uuid_v4();
+    std::map<std::string, simdjson::dom::element> pjson = *parsed_json.get();
+    pjson[uuid] = doc;
+    parsed_json.reset();
+    result.message = uuid.c_str();
+    result.status  = "OK\0";
   } else {
-	return 1;
+    result.message = "FAILED TO PARSE JSON\0";
+    result.status  = "ERROR\0";
   }
+  return result;
 }
 }
--- lib/Json/Simd.rakumod
+++ lib/Json/Simd.rakumod
@@ -6,37 +6,43 @@ use nqp;
 unit module JSON::Simd;
 
 constant $lib-location = %?RESOURCES<libraries/simd-json> // 'resources/libraries/libsimd-json.so';
-sub set_type_funcs(&add_uint64 (Str, uint64),
-				   &add_int64 (Str, int64),
-				   &add_str (Str, Str),
-				   &add_bool (Str, Bool),
-				   &add_double (Str, num64),
-				   &add_null (Str),
-				   &add_obj (Str),
-				   &add_array (Str)) is native($lib-location) { * }
-sub parse_json(Str:D $json) returns int32 is native($lib-location) { * }
+class parse_result is repr('CStruct') {
+    has Str $.message;
+    has Str $.status where * eq 'OK' || * eq 'ERROR';
+}
+sub delete_parsed_json(Str:D $uuid) returns void is native($lib-location) { * }
+sub parse_json(Str:D $json) returns parse_result is native($lib-location) { * }
+
+class JSON is Associative {
+    has Str $.id;
+
+    method AT-KEY($key) {
+        
+    }
+    
+    DESTROY {
+        delete_parsed_json($.uuid);
+    }
+}
 
 our sub parse(Str:D $json) is export {
-	my $*JSON = Nil;
-	if parse_json($json) == 0 {
-		if nqp::istype($*JSON, Array) {
-			return @$*JSON;
-		} elsif nqp::istype($*JSON, Associative) {
-			return %$*JSON;
-		} else {
-			return $*JSON;			
-		}
-	} else { return Nil }
+	my $resp = parse_json($json);
+
+    if $resp.status eq 'ERROR' {
+        die $resp.message;
+    } else {
+        return 
+    }
 }
 
 my sub add-value(Str:D $name, Any $value) {
-	nqp::unless($*JSON.defined,
-			nqp::stmts(($*JSON = $value),
+	nqp::unless($JSON.defined,
+			nqp::stmts(($JSON = $value),
 					   (return)));
 
 	state @prev-name = $name.split('␟', :skip-empty);
 
-	my $location = $*JSON;
+	my $location = $JSON;
 	my @name-parts = $name.split('␟', :skip-empty);
 	unless (@prev-name.elems == @name-parts.elems) &&
 	       (@prev-name.head(*-1) eqv @name-parts.head(*-1)) {
@@ -52,47 +58,3 @@ my sub add-value(Str:D $name, Any $value) {
 			nqp::stmts($location.push: $value),
 			$location{@name-parts[*-1]} = $value);
 }
-
-# These could all be replaced by a single func, but,
-# for the sake of changing this later I'm going to leave
-# them seperate
-sub add-uint64(Str:D $name, uint64 $value) {
-	add-value($name, $value);
-}
-
-sub add-int64(Str:D $name, int64 $value) {
-	add-value($name, $value);
-}
-
-sub add-str(Str:D $name, Str $value) {
-	add-value($name, $value);
-}
-
-sub add-bool(Str:D $name, int32 $value) {
-	add-value($name, ?$value);
-}
-
-sub add-double(Str:D $name, num64 $value) {
-	add-value($name, $value);
-}
-
-sub add-null(Str:D $name) {
-	add-value($name, Nil);
-}
-
-sub add-obj(Str:D $name) {
-	add-value($name, Hash.new);
-}
-
-sub add-array(Str:D $name) {
-	add-value($name, Array.new);
-}
-
-set_type_funcs(&add-uint64,
-			   &add-int64,
-			   &add-str,
-			   &add-bool,
-			   &add-double,
-			   &add-null,
-			   &add-obj,
-			   &add-array);
