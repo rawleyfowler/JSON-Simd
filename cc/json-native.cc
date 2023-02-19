1dc341c9f31d5e2447eaccb1eea08873723e0421
Rawley Fowler <rawleyfowler@gmail.com>
2023-02-18 22:50:44 -0600

    bare-bone-ify

--- cc/json-native.cc
+++ cc/json-native.cc
@@ -10,37 +10,37 @@
 #include "lib/simdjson/singleheader/simdjson.h"
 
 namespace uuid {
-    static std::random_device              rd;
-    static std::mt19937                    gen(rd());
-    static std::uniform_int_distribution<> dis(0, 5);
-    static std::uniform_int_distribution<> dis2(7, 17);
+static std::random_device rd;
+static std::mt19937 gen(rd());
+static std::uniform_int_distribution<> dis(0, 5);
+static std::uniform_int_distribution<> dis2(7, 17);
 
-    std::string generate_uuid_v4() {
-        std::stringstream ss;
-        int i;
-        ss << std::hex;
-        for (i = 0; i < 8; i++) {
-            ss << dis(gen);
-        }
-        ss << "-";
-        for (i = 0; i < 4; i++) {
-            ss << dis(gen);
-        }
-        ss << "-4";
-        for (i = 0; i < 3; i++) {
-            ss << dis(gen);
-        }
-        ss << "-";
-        ss << dis2(gen);
-        for (i = 0; i < 3; i++) {
-            ss << dis(gen);
-        }
-        ss << "-";
-        for (i = 0; i < 12; i++) {
-            ss << dis(gen);
-        };
-        return ss.str();
-    }
+std::string generate_uuid_v4() {
+  std::stringstream ss;
+  int i;
+  ss << std::hex;
+  for (i = 0; i < 8; i++) {
+    ss << dis(gen);
+  }
+  ss << "-";
+  for (i = 0; i < 4; i++) {
+    ss << dis(gen);
+  }
+  ss << "-4";
+  for (i = 0; i < 3; i++) {
+    ss << dis(gen);
+  }
+  ss << "-";
+  ss << dis2(gen);
+  for (i = 0; i < 3; i++) {
+    ss << dis(gen);
+  }
+  ss << "-";
+  for (i = 0; i < 12; i++) {
+    ss << dis(gen);
+  };
+  return ss.str();
+}
 }
 
 static std::shared_ptr<std::map<std::string, simdjson::dom::element>> parsed_json;
@@ -52,8 +52,8 @@ typedef struct parse_result {
 } parse_result;
 
 void delete_parsed_json(const char *uuid) {
-  auto pjson = *parsed_json.get();
-  pjson.erase(std::string(uuid));
+  parsed_json.get()->erase(uuid);
+  parsed_json.reset();
 }
 
 const parse_result parse_json(const char *json) {
@@ -64,8 +64,7 @@ const parse_result parse_json(const char *json) {
   parse_result result;
   if (error == simdjson::SUCCESS) {
     std::string uuid = uuid::generate_uuid_v4();
-    std::map<std::string, simdjson::dom::element> pjson = *parsed_json.get();
-    pjson[uuid] = doc;
+    (*parsed_json.get())[uuid] = doc;
     parsed_json.reset();
     result.message = uuid.c_str();
     result.status  = "OK\0";
--- lib/Json/Simd.rakumod
+++ lib/Json/Simd.rakumod
@@ -8,19 +8,15 @@ unit module JSON::Simd;
 constant $lib-location = %?RESOURCES<libraries/simd-json> // 'resources/libraries/libsimd-json.so';
 class parse_result is repr('CStruct') {
     has Str $.message;
-    has Str $.status where * eq 'OK' || * eq 'ERROR';
+    has Str $.status;
 }
 sub delete_parsed_json(Str:D $uuid) returns void is native($lib-location) { * }
 sub parse_json(Str:D $json) returns parse_result is native($lib-location) { * }
 
-class JSON is Associative {
+class JSON does Associative {
     has Str $.id;
-
-    method AT-KEY($key) {
-        
-    }
     
-    DESTROY {
+    method DESTROY {
         delete_parsed_json($.uuid);
     }
 }
@@ -28,33 +24,11 @@ class JSON is Associative {
 our sub parse(Str:D $json) is export {
 	my $resp = parse_json($json);
 
+    say $resp;
+
     if $resp.status eq 'ERROR' {
         die $resp.message;
     } else {
-        return 
+        return JSON.new(id => $resp.message);
     }
-}
-
-my sub add-value(Str:D $name, Any $value) {
-	nqp::unless($JSON.defined,
-			nqp::stmts(($JSON = $value),
-					   (return)));
-
-	state @prev-name = $name.split('␟', :skip-empty);
-
-	my $location = $JSON;
-	my @name-parts = $name.split('␟', :skip-empty);
-	unless (@prev-name.elems == @name-parts.elems) &&
-	       (@prev-name.head(*-1) eqv @name-parts.head(*-1)) {
-		@prev-name = @name-parts;
-		for @name-parts.head: *-1 {
-			nqp::if(nqp::istype($location, Array),
-					nqp::stmts($location = $location[$_.split('␝', :skip-empty)[0].Int]),
-					$location = $location{$_});
-		}
-	}
-
-	nqp::if(nqp::istype($location, Array),
-			nqp::stmts($location.push: $value),
-			$location{@name-parts[*-1]} = $value);
 }
